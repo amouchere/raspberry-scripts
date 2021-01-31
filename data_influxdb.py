4a31f26e546c3d78d8effbe2966ee7c9f1afbd39
amouchere <a.mouchere@protonmail.com>
2021-01-31 22:32:17 +0100

    :sparkles: add script to monitor disk size in influxdb

---
+++ data_influxdb.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# Python 3, prerequis : pip install pySerial influxdb
+
+
+
+import serial
+import time
+import requests
+from subprocess import check_output
+from datetime import datetime
+from influxdb import InfluxDBClient
+
+# connexion a la base de données InfluxDB
+client = InfluxDBClient('192.168.1.17', 8086)
+db = "homedata"
+connected = False
+while not connected:
+    try:
+        # print ("Database %s exists?", db)
+        if not {'name': db} in client.get_list_database():
+            # print ("Database %s creation..", db)
+            client.create_database(db)
+            # print ("Database %s created!", db)
+        client.switch_database(db)
+        # print ("Connected to ", db)
+    except requests.exceptions.ConnectionError:
+        # print ('InfluxDB is not reachable. Waiting 5 seconds to retry.')
+        time.sleep(5)
+    else:
+        connected = True
+
+
+def add_measures(datasize):
+    points = []
+    point = {
+                "measurement": "datasize",
+                "tags": {
+                    "host": "raspberry",
+                    "region": "linky"
+                },
+                "time": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
+                "fields": {
+                    "value": datasize
+                }
+            }
+    # print ("Database point ", point)
+    points.append(point)
+
+    client.write_points(points)
+
+
+datasize=check_output("sudo du -sh /home/pi | cut -d'M' -f1  | xargs", shell=True)
+
+# insertion dans influxdb
+add_measures(int(datasize.strip()))
+
+# result = client.query('select value from datasize;')
+
+# print("Result: {0}".format(result))
